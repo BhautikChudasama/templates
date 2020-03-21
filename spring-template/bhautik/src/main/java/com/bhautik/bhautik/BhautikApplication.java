e242c4a574b3d56780b67e20a479f90fceadec16
Bhautik <34828343+BhautikChudasama@users.noreply.github.com>
2020-03-21 11:48:18 +0530

    spring

---
+++ spring-template/bhautik/src/main/java/com/bhautik/bhautik/BhautikApplication.java
@@ -0,0 +1,13 @@
+package com.bhautik.bhautik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhautikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhautikApplication.class, args);
+	}
+
+}
---
+++ spring-template/bhautik/src/test/java/com/bhautik/bhautik/BhautikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhautik.bhautik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhautikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
